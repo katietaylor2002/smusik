164cc4f13ee26e74c461581225795ccfac615eb1
ktaylor <katietaylor150@rocketmail.com>
2024-05-09 14:04:21 +0100

    add grid

--- search.py
+++ search.py
@@ -2,8 +2,7 @@ import io
 import urllib.request
 from tkinter import *
 import customtkinter
-from PIL import Image, ImageTk
-import requests
+from PIL import Image
 
 import spotipy.util as util
 import spotipy
@@ -28,11 +27,6 @@ class Search:
         token = util.prompt_for_user_token(username, scope, CLIENT_ID, CLIENT_SECRET, redirect_uri)
         return spotipy.Spotify(auth=token)
 
-    @staticmethod
-    def get_song_choice():
-        print("select song")
-        return int(input())
-
     def search_for_song(self, search_textbox, window):
         song = search_textbox.get("1.0", "end-1c")
         search = self.sp.search(song, 5)
@@ -48,9 +42,13 @@ class Search:
 
         song_button_1 = customtkinter.CTkButton(window, text=search["tracks"]["items"][0]["name"] + " by " +
                                                              search["tracks"]["items"][0]["artists"][0]["name"],
-                                                font=("Impact", 20),
+                                                font=("Impact", 30),
+                                                height=50,
                                                 command=lambda: self.start_game(search["tracks"]["items"][0]["uri"],
                                                                                 window))
+
+        image_1.grid(row=3, column=1, columnspan=3, pady=40, sticky="EW")
+        song_button_1.grid(row=3, column=2, columnspan=3, sticky="EW")
 
         with urllib.request.urlopen(search["tracks"]["items"][1]["album"]["images"][0]["url"]) as u:
             raw_data = u.read()
@@ -63,9 +61,13 @@ class Search:
 
         song_button_2 = customtkinter.CTkButton(window, text=search["tracks"]["items"][1]["name"] + " by " +
                                                              search["tracks"]["items"][1]["artists"][0]["name"],
-                                                font=("Impact", 20),
+                                                font=("Impact", 30),
+                                                height=50,
                                                 command=lambda: self.start_game(search["tracks"]["items"][1]["uri"],
                                                                                 window))
+
+        image_2.grid(row=4, column=1, columnspan=3, pady=40, sticky="EW")
+        song_button_2.grid(row=4, column=2, columnspan=3, sticky="EW")
 
         with urllib.request.urlopen(search["tracks"]["items"][2]["album"]["images"][0]["url"]) as u:
             raw_data = u.read()
@@ -78,54 +80,13 @@ class Search:
 
         song_button_3 = customtkinter.CTkButton(window, text=search["tracks"]["items"][2]["name"] + " by " +
                                                              search["tracks"]["items"][2]["artists"][0]["name"],
-                                                font=("Impact", 20),
+                                                font=("Impact", 30),
+                                                height=50,
                                                 command=lambda: self.start_game(search["tracks"]["items"][2]["uri"],
                                                                                 window))
 
-        with urllib.request.urlopen(search["tracks"]["items"][3]["album"]["images"][0]["url"]) as u:
-            raw_data = u.read()
-
-        my_image = customtkinter.CTkImage(light_image=Image.open(io.BytesIO(raw_data)),
-                                          dark_image=Image.open(io.BytesIO(raw_data)),
-                                          size=(180, 180))  # WidthxHeight
-
-        image_4 = customtkinter.CTkLabel(window, text="", image=my_image)
-
-        song_button_4 = customtkinter.CTkButton(window, text=search["tracks"]["items"][3]["name"] + " by " +
-                                                             search["tracks"]["items"][3]["artists"][0]["name"],
-                                                font=("Impact", 20),
-                                                command=lambda: self.start_game(search["tracks"]["items"][3]["uri"],
-                                                                                window))
-
-        with urllib.request.urlopen(search["tracks"]["items"][4]["album"]["images"][0]["url"]) as u:
-            raw_data = u.read()
-
-        my_image = customtkinter.CTkImage(light_image=Image.open(io.BytesIO(raw_data)),
-                                          dark_image=Image.open(io.BytesIO(raw_data)),
-                                          size=(180, 180))  # WidthxHeight
-
-        image_5 = customtkinter.CTkLabel(window, text="", image=my_image)
-
-        song_button_5 = customtkinter.CTkButton(window, text=search["tracks"]["items"][4]["name"] + " by " +
-                                                             search["tracks"]["items"][4]["artists"][0]["name"],
-                                                font=("Impact", 20),
-                                                command=lambda: self.start_game(search["tracks"]["items"][4]["uri"],
-                                                                                window))
-
-        image_1.pack(pady=10)
-        song_button_1.pack(pady=5)
-
-        image_2.pack(pady=10)
-        song_button_2.pack(pady=5)
-
-        image_3.pack(pady=10)
-        song_button_3.pack(pady=5)
-
-        image_4.pack(pady=10)
-        song_button_4.pack(pady=5)
-
-        image_5.pack(pady=10)
-        song_button_5.pack(pady=5)
+        image_3.grid(row=5, column=1, columnspan=3, pady=40, sticky="EW")
+        song_button_3.grid(row=5, column=2, columnspan=3, sticky="EW")
 
     def start_game(self, song, window):
         window.destroy()
@@ -136,15 +97,15 @@ class Search:
         customtkinter.set_appearance_mode("dark")
         customtkinter.set_default_color_theme("green")
         window = customtkinter.CTk()
+        window.geometry("800x1000")
 
         heading = customtkinter.CTkLabel(window, text="SMUSIC", font=("Impact", 40))
-        search_textbox = customtkinter.CTkTextbox(window, height=100, width=400, font=("Impact", 40))
+        search_textbox = customtkinter.CTkTextbox(window, height=50, width=400, font=("Impact", 40))
         search_button = customtkinter.CTkButton(window, text="search", font=("Impact", 30),
                                                 command=lambda: self.search_for_song(search_textbox, window))
 
-        heading.pack(pady=10)
-        search_textbox.pack(pady=10)
-        search_button.pack(pady=10)
+        heading.grid(row=0, column=3, padx=200, pady=20)
+        search_textbox.grid(row=1, column=3, padx=200, pady=5)
+        search_button.grid(row=2, column=3, padx=200, pady=5)
 
-        window.geometry("800x1000")
         window.mainloop()
